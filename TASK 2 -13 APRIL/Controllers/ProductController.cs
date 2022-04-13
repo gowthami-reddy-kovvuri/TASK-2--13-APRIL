d9d39e25e2a1aa458e0b804ecd368a998b3c8441
GOWTHAMI REDDY <GOWTHAMI REDDY@DESKTOP-JSSSRLJ>
2022-04-13 20:10:24 +0530

    Add project files.

---
+++ TASK 2 -13 APRIL/Controllers/ProductController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TASK_2__13_APRIL.Models;
+namespace TASK_2__13_APRIL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        // GET: api/<ProductController>
+        [HttpGet]
+        public IEnumerable<Productdet> Get()
+        {
+            DB3Context pnt = new DB3Context();
+            return pnt.Productdet;
+        }
+
+        // GET api/<ProductController>/5
+        [HttpGet("{pcode}")]
+        public IEnumerable<Productdet> Get(int pcode)
+        {
+            DB3Context pnt = new DB3Context();
+            var sql = from i in pnt.Productdet where i.Pcode == pcode select i;
+            return sql;
+        }
+        // POST api/<ProductController>
+        [HttpPost]
+        public void Post([FromBody] Productdet value)
+        {
+            DB3Context pnt = new DB3Context();
+            pnt.Productdet.Add(value);
+            pnt.SaveChanges();
+        }
+
+        // PUT api/<ProductController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Productdet value)
+        {
+            DB3Context pnt = new DB3Context();
+            var det = pnt.Productdet.Find(id);
+            if (det != null)
+            {
+                det.Pname = value.Pname;
+                det.Pdesc = value.Pdesc;
+                det.Unitprice = value.Unitprice;
+                det.Category = value.Category;
+                det.Stockinhand = value.Stockinhand;
+                pnt.SaveChanges();
+            }
+        }
+
+        // DELETE api/<ProductController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            DB3Context pnt = new DB3Context();
+            var del = pnt.Productdet.Find(id);
+            pnt.Productdet.Remove(del);
+            pnt.SaveChanges();
+        }
+    }
+}
+
+
+
---
+++ TASK 2 -13 APRIL/Models/Startup.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TASK_2__13_APRIL.Models;
+using Microsoft.EntityFrameworkCore;
+namespace TASK_2__13_APRIL.Models
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+       
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddDbContext<DB3Context>(option => option.UseSqlServer(Configuration.GetConnectionString("DB3CON")));
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Title = "Swagger demo",
+                    Description = "swagger demo for product details",
+                    Version = "v1"
+                });
+            });
+            services.AddCors(c =>
+            {
+                c.AddPolicy("AllowOrigin", option => option.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            });
+
+        }
+
+      
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Staffdet-api"));
+
+            }
+            app.UseCors(op => op.AllowAnyOrigin());
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
